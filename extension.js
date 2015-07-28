4abaf8d769361ca0fd3985a1311589e8ec4968f2
testcaraio <ckisdjs@hotmail.com>
2015-07-28 04:41:41 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -57,8 +57,8 @@
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
         botName: "basicBot",
-        language: "english",
-        chatLink: "https://rawgit.com/testcaraio/basicBot/master/lang/en.json",
+        language: "portuguese",
+        chatLink: "https://rawgit.com/testcaraio/basicBot/master/lang/pt-BR.json",
         startupCap: 1, // 1-200
         startupVolume: 0, // 0-100
         startupEmoji: false, // true or false
